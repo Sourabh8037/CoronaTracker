9809ae14c98592949c700fb796944d339cfbe23a
Sourabh <hande.sourabh@gmail.com>
2020-07-28 20:20:25 +0530

    Updates

---
+++ precache-manifest.b039148c221f7e7f2e90dd4907dfa3b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8862b8a16e703896df6088f4c365ae",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "3e6f445bf0b17a9a52a2",
+    "url": "/CoronaTracker/static/css/main.48185a5f.chunk.css"
+  },
+  {
+    "revision": "34dbc49a978f9c07582b",
+    "url": "/CoronaTracker/static/js/2.2b0c2bc1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoronaTracker/static/js/2.2b0c2bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6f445bf0b17a9a52a2",
+    "url": "/CoronaTracker/static/js/main.e840d49f.chunk.js"
+  },
+  {
+    "revision": "70702e1617ad81d0fbc9",
+    "url": "/CoronaTracker/static/js/runtime-main.678dd8cd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoronaTracker/static/media/image.d7265326.png"
+  }
+]);
